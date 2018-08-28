4409ca183e180862df97c4b9bb39f4ff0b8fcf59
Moriyama Masataka <masataka.312@gmail.com>
2018-08-28 15:28:12 +0900

    メニュー画面　開かせる

--- resource/js/scripts.js
+++ resource/js/scripts.js
@@ -2,7 +2,13 @@
 
 // html読み込み完了時
 $(function(){
+	// ハンバーガーメニューのアニメーション	
 	$('.menu-trigger').click(function() {
 		$(this).toggleClass('active');
+	});
+
+	// メニュー表示
+	$('.menu-trigger').click(function() {
+		$('#overwrp-menu').toggleClass('opened');
 	});
 });
